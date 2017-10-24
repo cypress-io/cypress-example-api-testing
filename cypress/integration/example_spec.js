a42c7935afeb3d01097736bb06426a7161405710
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-23 23:39:23 -0400

    two more tests

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,5 +1,20 @@
 /* eslint-env mocha */
 describe('todos API', () => {
+  const initialItems = [
+    {
+      "id": 1,
+      "task": "read something"
+    },
+    {
+      "id": 2,
+      "task": "write something"
+    }
+  ]
+
+  const getItems = () =>
+    cy.request('/todos')
+      .its('body')
+
   it('returns JSON', () => {
     cy.request('/todos')
       .its('headers')
@@ -8,8 +23,19 @@ describe('todos API', () => {
   })
 
   it('loads 2 items', () => {
-    cy.request('/todos')
-      .its('body')
+    getItems()
       .should('have.length', 2)
+  })
+
+  it('loads the initial items', () => {
+    getItems()
+      .should('deep.eq', initialItems)
+  })
+
+  it('returns id + task objects', () => {
+    getItems()
+      .each(value =>
+        expect(value).to.have.all.keys('id', 'task')
+      )
   })
 })
